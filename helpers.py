76d877db20f0750d5680be0a181ca224e07760bc
iamserda <iamserda@gmail.com>
2023-01-18 23:53:09 -0500

    2023-18-01:2351: completed version 1.2 of the app. Tested the application for some common errors and added error handling. Added some comments to the different procedures to better explain my approach. -iamserda

--- helpers.py
+++ helpers.py
@@ -1,148 +1,163 @@
 import json
 
 
-def add():
-    """ Prompts user for a new item.
-    adds it to the list
-            returns true once completed.
-    """
+def add(new_item=None):
+    """ allows users to ADD a new item to the end of the 'open-tasks' list."""
     try:
         # read any data that was already saved, move a copy to memory.
-        read_fs = open('storage/data_store.json', 'r')
-        data_store = json.load(read_fs) #this is a python dictionary object
+        data_store = get_data_store('storage/data_store.json')
         open_tasks = data_store['open-tasks']
-        read_fs.close()
-        
-        #get the new list item from the user, save it into open-tasks.
-        message = "Enter item to be added to list.\nEx: 'Buy a carton of eggs!': "
-        user_prompt = input(message).strip()
-        if not user_prompt:
-            message = "Message: Could not add a new item. User did not enter any value. No changes made."
-            raise ValueError(message)
-        open_tasks.append(user_prompt)
-        
+        # get the new list item from the user, save it into open-tasks.
+        if new_item:
+            open_tasks.append(new_item)
         # save the open-tasks to a json file.
-        write_fs = open('storage/data_store.json', 'w')
-        json.dump(data_store, write_fs, indent=4)
-        write_fs.close()
+        set_data_store(data_store)
         return True
+
     except Exception as err:
         # if there were errors, print them.
         print(f"Error: {err}")
         return False
 
 
-def edit():
-    """ edit:
-        a function to edit an item from the list.
-        if the list is empty, it warns the user that editing can only be done on a list that has items.
-        else it prints the items, and allow user to select which item should be edited.
+def basic_editing():
+    """ 
+        ex: edit 1 Return the bad fruit basket to the grocery store.
+
+        Allows user to edit an item based on its position on the list of open tasks.
+
+        If the list is empty, meaning there are no tasks, it warns the user that editing can only be done on a list.
+
+        Otherwise, it tries locates which items user wants to edit, and updates it accordingly.
+
+        In case where the item, index or location, is out of bound or not available, it throws an error, and returns False.
     """
     try:
-    # open file in read mode:
-        read_fs = open('storage/data_store.json', 'r')
-        data_store = json.load(read_fs)
+        # open file in read mode:
+        data_store = get_data_store()
         tasks_store = data_store['open-tasks']
-        read_fs.close()
-    # editing logic:
+        # editing logic:
         if not len(tasks_store):
             print("Your current list is empty. You can't edit an empty list")
             print("Please add items, and try again.")
             return False
 
-        show_open_tasks()
-        
+        show_open_tasks(tasks_store)
+
         # ask user which item to edit
         message = """Which item would you like to edit or replace?\n
         Please enter an integer only. Ex: 1 for the first item: """
         user_input = input(message).strip()
-        
+
         if not user_input.isnumeric():
             message = "User input was an invalid data type.\nPlease enter integer only."
             raise TypeError(message)
-        
+
         index = int(user_input) - 1
         if index < 0 or index > len(tasks_store) - 1:
             message = "Invalid Index! Either less than 0 or more than number of open tasks."
             raise ValueError(message)
-        
+
         print(tasks_store[index])
         user_input = input("Enter new value: ")
         if not user_input:
-            raise ValueError("User didn't enter anything. Item cannot be blank.")
+            raise ValueError(
+                "User didn't enter anything. Item cannot be blank.")
         tasks_store[index] = user_input
     # save the changes into the json file.
-        write_fs = open('storage/data_store.json', 'w')
-        json.dump(data_store, write_fs, indent=4)
-        write_fs.close()
+        set_data_store(data_store)
         return True
     except Exception as err:
         print(f'Error: {err}')
         return False
 
 
-def mark_complete():
+def complex_editing(setup_arr=list()):
+    """ 
+    ex: edit 1 Return the bad fruit basket to the grocery store.
+
+    Allows user to edit an item based on its position on the list of open tasks.
+
+    If the list is empty, meaning there are no tasks, it warns the user that editing can only be done on a list.
+
+    Otherwise, it tries locates which items user wants to edit, and updates it accordingly.
+
+    In case where the item, index or location, is out of bound or not available, it throws an error, and returns False.
+    """
+    try:
+        data_store = get_data_store()
+        tasks_store = data_store['open-tasks']
+
+        if not setup_arr or len(setup_arr) != 3:
+            raise ValueError(
+                """Please enter the number you want to change, followed by the new value you wish to use as its replacement. For example:\n ex: edit 2 Miami Dolphins """)
+
+        if len(setup_arr) == 3:
+            index = int(setup_arr[1]) - 1
+            message = setup_arr[2]
+            if index > len(tasks_store) or index < 0:
+                raise ValueError(f"Invalid position. Next possible location is {len(tasks_store)}.")
+            tasks_store[index] = message
+            set_data_store(data_store)
+        else:
+            raise ValueError("")
+        return True
+    except Exception as err:
+        print(f"Error: {err}")
+        return False
+
+
+def mark_complete(index=-1):
     """ Allow users to mark an item on the open tasks list completed. Moves completed task to a different list named "completed-tasks-list." Completed tasks are saved for historical purposes.
 
     Args:
         tasks_store (list/array): list containing all the open task currently available
-        to the user. Task that haven't been completed.
-    """
-    read_fs = open('storage/data_store.json')
-    data_store = json.load(read_fs)
+        to the user. Task that haven't been completed."""
+
+    data_store = get_data_store('storage/data_store.json')
     tasks_store = data_store['open-tasks']
     completed_store = data_store['completed-tasks']
-    read_fs.close()
-    
+
     if not len(tasks_store):
         print("Your current list is empty. \nPlease add items, and try again.")
         return False
-    
+
     try:
-        show_open_tasks()
-        user_input = input( 'Select an item from the list above. Use integer only: 1, 2, 3...: ').strip()
-        index = int(user_input) if user_input.isnumeric() and int(user_input) < len(tasks_store) else -1
-        index = index - 1
-        if index < 0 or index > len(tasks_store)-1:
-            raise ValueError("Invalid Index!")
-        completed_store.append( tasks_store.pop(index-1) )
-        write_fs = open('storage/data_store.json', 'w')
-        json.dump(data_store, write_fs, indent=4)
-        write_fs.close()
+        if not index:
+            raise Exception("Failed to provide a valid index")
+        
+        index = int(index) - 1
+        if index < 0 or index > len(tasks_store) - 1:
+            raise Exception(f"The position provided is not valid! Select between 1 and {len(tasks_store)}")
+
+        completed_store.append(tasks_store.pop(index))
+        set_data_store(data_store)
         return True
     except Exception as err:
         print(f'Error: {err}')
         return False
 
 
-def exit():
-    """
-    Ends the applications on user's request. Takes no arguments. Always returns False. 
-    False should be used to terminate the program inside of main.
-    """
-    print('exiting...')
-    print("Ending Program at User's request!")
-    return False
-
-
-def show_open_tasks():
+def show_open_tasks(task_store=None):
     """Iterates over a items list and displays each value to the usesr.
     Args: items: a python list where we store each tasks as user adds, modifies and delete values.
     Returns:
         bool: _description_
     """
-    read_fs = open("storage/data_store.json", "r")
-    data_store = json.load(read_fs)
-    items = data_store['open-tasks']
-    
-    if not len(items):
-        return False
-    
-    print(f"List Size: {len(items)}")
-    print("List Items: ")
-    for index, value in enumerate(items):
-        print(f'{index + 1} - {value}')
-    return True
+    try:
+        data_store = get_data_store()
+        items = data_store['open-tasks']
+
+        if not len(items):
+            return False
+        print("List Items: ")
+        for index, value in enumerate(items):
+            print(f'{index + 1} - {value}')
+        print("\n")
+        print(f"Size: {len(items)}\n")
+        return True
+    except Exception as err:
+        print(f"Error: {err}")
 
 
 def show_completed_tasks():
@@ -155,15 +170,16 @@ def show_completed_tasks():
     read_fs = open("storage/data_store.json", "r")
     data_store = json.load(read_fs)
     items = data_store['completed-tasks']
-    
+
     if not len(items):
         print("You haven't completed any tasks recently.")
         return False
-    print( f"Completed Tasks List: \nSize: {len(items)}\nItems:\n")
+    print(f"Completed Tasks List: \nSize: {len(items)}\nItems:\n")
     for index, value in enumerate(items):
-            print(f'{index + 1} - {value}')
+        print(f'{index + 1} - {value}')
     print("")
     return
+
 
 def show_list_size(tasks_store):
     """
@@ -172,3 +188,16 @@ def show_list_size(tasks_store):
         tasks_store (list/array): list containing all the open task currently available
         to the user. Task that haven't been completed."""
     print(f"Open Items: {len(tasks_store)}")
+
+
+def get_data_store(file_path='storage/data_store.json'):
+    read_fs = open(file_path, 'r')
+    data_store = json.load(read_fs)  # this is a python dictionary object
+    read_fs.close()
+    return data_store
+
+
+def set_data_store(data_store, file_path='storage/data_store.json'):
+    write_fs = open(file_path, 'w')
+    json.dump(data_store, write_fs, indent=4)
+    write_fs.close()
--- main.py
+++ main.py
@@ -1,76 +1,101 @@
-from helpers import add, edit, mark_complete, exit as x, show_completed_tasks as showc, show_open_tasks as showo
+from helpers import add, basic_editing, complex_editing, mark_complete
+from helpers import show_completed_tasks as showc, show_open_tasks as showo
 
 
 def main():
     """
     summary: 
     Daily task list app. Keep a tab on your day via the command line.
-
+    
+    To add a new item: 'add' or '1'
+    
+    To modify an item: 'edit' or '2'
+    
+    To complete a task: 'comp' or '3' 
+    
+    To show open tasks: 'show', 'showo', 'show -o' or '4'
+    
+    To show completed tasks: 'showc', 'show -c', or '5'
+    
+    To exit: 'x', 'exit', '0'
+    
     Raises:
         ValueError: 
         If user enters something that's not an option on the menu, 
         we raise a ValueError and offer the user the option to try again or end the program.
     Returns None.
     """
-    menu_option_prompt = """
-Select an option from this menu:
-    '1' or 'add'            - To add a new item.
-    '2' or 'edit'           - To modify an item.
-    '3' or 'complete'       - To mark a task as completed.
-    '4' or 'show -open'           - To show the list of open tasks.
-    '5' or 'show -completed'- To show the list of completed tasks.
-    '9' or 'x' or 'exit'    - To exit.\n"""
+    menu = """Select an option from this menu:
+    To add a new item: 'add' or '1'
+    To modify an item: 'edit' or '2'
+    To complete a task: 'comp' or '3' 
+    To show open tasks: 'show', 'showo', 'show -o' or '4'
+    To show completed tasks: 'showc', 'show -c', or '5'
+    To exit: 'x', 'exit', '0'\n"""
 
     flag = True
-    
-    while flag:
-        try:
+
+    try:
+        while flag:
             user_input = input(
-                "".join([menu_option_prompt, "\nPlease enter your selection: "]))
+                "".join([menu, "\nPlease enter your selection: "]))
             if not user_input:
                 local_message = "You did not enter a value.\nNo changes made, ending Program!"
                 raise ValueError(local_message)
 
-            match user_input:
-                case 'x':
-                    flag = x()
-                case '0':
-                    flag = x()
-                case'exit':
-                    flag = x()
-                case '1':
-                    flag = add()
-                case 'add':
-                    flag = add()
-                case '2':
-                    flag = edit()
-                case 'edit':
-                    flag = edit()
-                case '3':
-                    flag = mark_complete()
-                case 'complete':
-                    flag = mark_complete()
-                case '4':
-                    flag = showo()
-                case 'show -open':
-                    flag = showo()
-                case '5':
-                    showc()
-                case 'show -completed':
-                    showc()
-                case _:
-                    # ~ default case:
-                    raise ValueError("'_' is not a valid value.")
-            if not flag:
-                raise TypeError("User input error! see previous message.")
-            print("Message: Success!")
-            continue
+            user_input.strip()
 
-        except Exception as err:
-            print("Error:", err)
-            flag = True if input('Would you like to try again? Enter Yes or No: ').lower() == 'yes' else False
-            if not flag:
-                print("Ending Program...")
+            if user_input[0] in ['x', '0', '1', '2', '3', '4', '5']:
+                match user_input[0]:
+                    case 'x':
+                        print("Ending on user's request...")
+                        break
+                    case '0':
+                        print("Ending on user's request...")
+                        break
+                    case '1':
+                        new_item = user_input[2:].strip()
+                        flag = add(new_item)
+                    case '2': flag = basic_editing()
+                    case '3': flag = mark_complete()
+                    case '4': flag = showo()
+                    case '5': flag = showc()
+                    case _:
+                        # ~ default case:
+                        raise ValueError("'_' is not a valid value.")
+            elif user_input.startswith('add'):
+                new_item = user_input[3:].strip()
+                if new_item:
+                    flag = add(new_item)
+
+            elif user_input.startswith('exit'):
+                print("Ending on user's request...")
                 break
-            continue
+            elif user_input.startswith(('comp', 'complete')):
+                setup_arr = user_input.split(" ", maxsplit=2)
+                if len(setup_arr) < 2:
+                    raise ValueError("User did not provide an index.")
+                if not setup_arr[1].isdigit():
+                    raise ValueError(
+                        "Invalid Index Value provided. Must be a integer.")
+                mark_complete(setup_arr[1])
+            elif user_input.startswith('edit'):
+                # expecting: ['edit', 'index', 'new value'] from the next line.
+                setup_arr = user_input.split(" ", maxsplit=2)
+                flag = complex_editing(setup_arr)
+            elif user_input.startswith('show'):
+                match user_input:
+                    case 'show': flag = showo()
+                    case 'show -o': flag = showo()
+                    case 'show -open': flag = showo()
+                    case 'showc': flag = showc()
+                    case 'show -c': flag = showc()
+                    case 'show -completed': flag = showc()
+            if not flag:
+                raise Exception(
+                    "Failed to execute your command! Exiting now...")
+    except Exception as err:
+        print("Error:", err)
+
+
 main()
